6a794de6ee20bb214fd2c05aeed2da333005b6e6
Francisco <javiju91077@hotmail.com>
2018-06-11 15:22:34 -0500

    cambio

--- F1app/F1app/F1app/CrearPiloto.xaml.cs
+++ F1app/F1app/F1app/CrearPiloto.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using F1app.Model;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,9 +15,61 @@ namespace F1app
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CrearPiloto : ContentPage
     {
-        public CrearPiloto ()
+        private const string Url = "https://javierjdapiproyectofinal.herokuapp.com/pilotos";
+        private readonly HttpClient client = new HttpClient();
+
+        public CrearPiloto()
         {
             InitializeComponent();
+        }
+
+        public void ClickButtonCreatePiloto(object sender, EventArgs e)
+        {
+            CreatePiloto();
+        }
+
+        async public void CreatePiloto()
+        {
+            Pilotos piloto = new Pilotos()
+            {
+                Name = entryName.Text,
+                Numero =  Convert.ToInt32(entryNumero.Text),
+                LastName = entryLastName.Text,
+                Team = entryEscuderia.Text,
+                Country = entryPais.Text,
+                DateBirth = entryFechaNacimiento.Text,
+                PlaceBirth = entryLugarNacimiento.Text
+
+            };
+
+            var json = JsonConvert.SerializeObject(piloto);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = null;
+            response = await client.PostAsync(Url, content);
+
+            entryName.Text = "";
+            entryLastName.Text = "";
+            entryNumero.Text = "";
+            entryEscuderia.Text = "";
+            entryFechaNacimiento.Text = "";
+            entryLugarNacimiento.Text = "";
+            entryPais.Text = "";
+
+            await DisplayAlert("Felicidades", "La pista fue creada correctamente", "OK");
+
+        }
+
+        public void CancelarRegresar()
+        {
+            entryName.Text = "";
+            entryLastName.Text = "";
+            entryNumero.Text = "";
+            entryEscuderia.Text = "";
+            entryFechaNacimiento.Text = "";
+            entryLugarNacimiento.Text = "";
+            entryPais.Text = "";
+
         }
     }
 }
